a316260414144c29cb476691b4ea4f74b4f71bc5
blew <blew@astro.uni.torun.pl>
2020-01-20 17:13:04 +0100

    update

--- install_services_ubuntu.sh
+++ install_services_ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp RT32logging/save-electric-cabin-data.service /lib/systemd/system/
+cp python/RT32logging/save-electric-cabin-data.service /lib/systemd/system/
 chown root:root /lib/systemd/system/save-electric-cabin-data.service
 
 systemctl enable save-electric-cabin-data
