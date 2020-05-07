189cedfd3fc526e919d99f74f5d19dccaf6d1c9c
blew <blew@astro.uni.torun.pl>
2020-05-07 11:36:59 +0200

    config update

--- install_services_ubuntu.sh
+++ install_services_ubuntu.sh
@@ -4,7 +4,7 @@ chown root:root /lib/systemd/system/save-electric-cabin-data.service
 
 systemctl enable save-electric-cabin-data
 
-cp python/save-electric-cabin-data/save-focus-box-meteo.service /lib/systemd/system/
+cp python/save-focus-box-meteo/save-focus-box-meteo.service /lib/systemd/system/
 chown root:root /lib/systemd/system/save-focus-box-meteo.service
 
 systemctl enable save-focus-box-meteo
